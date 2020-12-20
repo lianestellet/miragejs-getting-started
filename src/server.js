4e3d6ffdcad275b36c344ac7ba81484bac0085fc
pablostellet <pablorstellet@gmail.com>
2020-12-20 11:25:18 -0300

    making the data saved dynamic

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 export default function () {
     createServer({
-        routes() {
-            this.get("/api/reminders", () => ({
-                reminders: [
-                    { id: 1, text: "Walk the dog" },
-                    { id: 2, text: "Take out the trash" },
-                    { id: 3, text: "Work out" },
-                ],
-            }))
 
-            let newId = 4
+        models: {
+            reminder: Model
+        },
+
+        routes() {
+            this.get("/api/reminders", (schema) => {
+               return schema.reminders.all()
+            })
+
             this.post("/api/reminders", (schema, request) => {
                 let attrs = JSON.parse(request.requestBody)
-                attrs.id = newId++
-                
-                return { reminder : attrs }
+
+                return schema.reminders.create(attrs)
             })
         }
     })
